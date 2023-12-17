4d90464d75b223566b81329537b851d615b9dbc9
Hedin <spiridonov.yury@gmail.com>
2023-12-17 10:21:47 +0100

    Docker configurations

--- SampeApp.Database.Migrations/Program.cs
+++ SampeApp.Database.Migrations/Program.cs
@@ -1,8 +1,9 @@
 ﻿using FluentMigrator.Runner;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Start DB migration");
+//Console.WriteLine("Start DB migration");
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -10,6 +11,7 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", false)
     .Build();
 
+var databaseName = "SampleAppDb";
 
 var connectionString = configuration.GetConnectionString("SampleAppDb");
 
@@ -22,6 +24,24 @@ var serviceCollection = new ServiceCollection()
     )
     .AddLogging(log => log.AddFluentMigratorConsole())
     .BuildServiceProvider();
+
+
+var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+connectionStringBuilder.Remove("initial catalog");
+connectionStringBuilder.Remove("database");
+using var connection = new SqlConnection(connectionStringBuilder.ConnectionString);
+var checkDbExists = new SqlCommand($"SELECT database_id FROM sys.databases WHERE Name = '{databaseName}'",
+    connection);
+connection.Open();
+var result = checkDbExists.ExecuteScalar();
+if (result is null)
+{
+    using var command = new SqlCommand($"CREATE DATABASE {databaseName}", connection);
+    command.ExecuteNonQuery();
+    Console.WriteLine($"Database {databaseName} created successfully!");
+}
+connection.Close();
+connection.Dispose();
 
 var runner = serviceCollection.GetRequiredService<IMigrationRunner>();
 
